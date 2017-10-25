3dcca24058cc9f8c81223965ed70062163e03261
2Deo <ziemowit.holda@gmail.com>
2017-10-25 10:14:02 +0200

    Add Student model, admin:boolean to User

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 20171023091428) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.boolean "admin"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
