fb8716eb2ae3d5ceb203f144ede7b7c0a61063e0
2Deo <ziemowit.holda@gmail.com>
2017-10-25 10:17:29 +0200

    Add association between User and Students

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
-rails class User < ApplicationRecord
+class User < ApplicationRecord
+  has_many :students
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
