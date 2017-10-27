139aac7005715ac9988723418bc8fafb6d321414
Wayne Louwrens <wayne.louwrens@gmail.com>
2017-10-27 14:54:05 +0200

    Show page matches

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
 
       def show
         @user = User.find(params[:id])
-        @students = User.students
+        @students = User.generate_pairs
+
       end
 
       def update
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,36 +6,43 @@ class User < ApplicationRecord
 
 # 1. Select all the users, not admins
 
-     def self.students
-       @students = User.all.select { |user| !user.admin }
-       #@students.pluck(:name)
-     end
+       def self.students
+         @students = User.all.select { |user| !user.admin }
+       end
 
-     #def remove_first_student
-      #  @sliced_student = @students.slice!(0)
-      #  @sliced_student_array = []
-        #@sliced_student_array << @sliced_student   #This is pushing the string of the sliced student to an array...
-     #end
+# 2. Create a pool of students by name
+      def self.student_pool
+          User.students
+          @student_pool_array = []
+          @students.each {|student| @student_pool_array << student.name}
+        end
 
 # 3. Generate the pairs
-    def self.generate_pairs
+      def self.generate_pairs
+        student_pool
+        @sliced_student = @student_pool_array.slice!(0)
 
-    end
+        @sliced_student_array = []
+        @sliced_student_array << @sliced_student   #This is pushing the string of the sliced student to an array...
 
-    # generate_sliced_pairs
-      #@day_one = @matched_pairs_array.slice!(0)
-      #@day_two = @matched_pairs_array.slice!(0)
-      #@day_three = @matched_pairs_array.slice!(0)
-      #@day_four = @matched_pairs_array.slice!(0)
-      #@day_five = @matched_pairs_array.slice!(0)
-    #end
+        @matched_pairs_array = @student_pool_array.product(@sliced_student_array)
+        return @matched_pairs_array
+      end
+
+      def generate_sliced_pairs
+        @day_one = @matched_pairs_array.slice!(0)
+        @day_two = @matched_pairs_array.slice!(0)
+        @day_three = @matched_pairs_array.slice!(0)
+        @day_four = @matched_pairs_array.slice!(0)
+        @day_five = @matched_pairs_array.slice!(0)
+      end
+
+  # 4. Slice our pairs
 
 
 
-
-
-    def is_admin?
-      self.admin?
-    end
+      def is_admin?
+        self.admin?
+      end
 
 end
